e12f78d10d7c992e6cb9748c6723a5e42bedf5c2
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-06-19 15:11:59 +0200

    Setup GitHub action workflows (#1)

---
+++ tests/TestCase.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Jonassiewertsen\Jobs\Tests;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Statamic\Extend\Manifest;
+use Statamic\Facades\Blueprint;
+use Statamic\Stache\Stores\UsersStore;
+use Statamic\Statamic;
+
+class TestCase extends OrchestraTestCase
+{
+    use DatabaseMigrations;
+    use WithFaker;
+    use PreventSavingStacheItemsToDisk;
+
+    protected $shouldFakeVersion = true;
+
+    public function tearDown(): void
+    {
+        $this->deleteFakeStacheDirectory();
+
+        parent::tearDown();
+    }
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        require_once __DIR__.'/Kernel.php';
+
+        parent::setUp();
+
+        $this->preventSavingStacheItemsToDisk();
+
+        if ($this->shouldFakeVersion) {
+            \Facades\Statamic\Version::shouldReceive('get')->andReturn('3.1.0-testing');
+            $this->addToAssertionCount(-1); // Dont want to assert this
+        }
+    }
+
+    /**
+     * Load package service provider.
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Statamic\Providers\StatamicServiceProvider::class,
+            \Jonassiewertsen\Jobs\ServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Load package alias.
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Statamic' => Statamic::class,
+        ];
+    }
+
+    /**
+     * Load Environment.
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app->make(Manifest::class)->manifest = [
+            'jonassiewertsen/statamic-jobs' => [
+                'id'        => 'jonassiewertsen/statamic-jobs',
+                'namespace' => 'Jonassiewertsen\\Jobs\\',
+            ],
+        ];
+    }
+
+    /**
+     * Resolve the Application Configuration and set the Statamic configuration.
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function resolveApplicationConfiguration($app)
+    {
+        parent::resolveApplicationConfiguration($app);
+
+        $configs = [
+            'assets', 'cp', 'routes', 'static_caching', 'sites', 'stache', 'system',
+        ];
+
+        foreach ($configs as $config) {
+            $app['config']->set("statamic.$config", require(__DIR__."/../vendor/statamic/cms/config/{$config}.php"));
+        }
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jonassiewertsen\Jobs\Tests\Unit;
+
+use Jonassiewertsen\Jobs\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
