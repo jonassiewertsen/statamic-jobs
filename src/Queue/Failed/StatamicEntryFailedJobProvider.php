f40712f9f4c346e5be171c82c6e6552ab96ef8ea
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-06-20 13:00:21 +0200

    Use a tap function

--- src/Queue/Failed/StatamicEntryFailedJobProvider.php
+++ src/Queue/Failed/StatamicEntryFailedJobProvider.php
@@ -32,7 +32,7 @@ class StatamicEntryFailedJobProvider implements FailedJobProviderInterface
         $uuid = json_decode($payload, true)['uuid'];
         $now = Date::now();
 
-        $job = Entry::make()
+         $job = tap(Entry::make()
             ->collection('failed_jobs')
             ->blueprint('failed_job')
             ->slug($this->slug($uuid, $now))
@@ -43,11 +43,9 @@ class StatamicEntryFailedJobProvider implements FailedJobProviderInterface
                 'payload' => $payload,
                 'exception' => $exception,
                 'failed_at' => $now,
-            ]);
+            ]))->save();
 
-        $job->save();
-
-        return $uuid;
+        return $job->id();
     }
 
     /**
