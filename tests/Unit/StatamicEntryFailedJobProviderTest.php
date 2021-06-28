9a623f6991d0c361e808652827ffd19006bead57
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-06-28 23:45:53 +0200

    only prefer stable at the moment

--- tests/Unit/StatamicEntryFailedJobProviderTest.php
+++ tests/Unit/StatamicEntryFailedJobProviderTest.php
@@ -35,7 +35,7 @@ class StatamicEntryFailedJobProviderTest extends TestCase
 
         $this->assertEquals($job->uuid, $uuid);
         $this->assertEquals($job->failed_at, $now->toIso8601String());
-        $this->assertEquals((string) $job->exception, (string) $exception);
+        $this->assertEquals($job->exception, (string) $exception);
     }
 
     /** @test */
